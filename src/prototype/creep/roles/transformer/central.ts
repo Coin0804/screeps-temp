812704333c54fb6203f48b7b0758329d3b7e8fb0
coin0804 <coin0804@outlook.com>
2022-04-19 18:13:02 +0800

    修改一些bug

--- src/prototype/creep/roles/transformer/central.ts
+++ src/prototype/creep/roles/transformer/central.ts
@@ -1,6 +1,6 @@
 
 export function run_as_transformer_central(creep:Creep){
-    creep.room.find(FIND_MY_SPAWNS)[0].renewCreep(this);
+    creep.room.find(FIND_MY_SPAWNS)[0].renewCreep(creep);
     let link:StructureLink = <StructureLink>creep.room.find(FIND_STRUCTURES,{filter:(s) => {
         return s.structureType == STRUCTURE_LINK && creep.pos.getRangeTo(s) == 1;
     }})[0];
